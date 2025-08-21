a034b68bf24c434fb28d7e220b5e9615e4587802
Harshcodesfast <harshawalegaonkar@gmail.com>
2025-08-21 17:11:53 +0530

    worflow done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import NavBar from "./conponents/navBar";
+import NavBar from "./conponents/NavBar";
 import HeroSection from "./conponents/heroSection";
 import FeatureSection from "./conponents/FeatureSection";
+import WorkFlow from "./conponents/WorkFlow";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
         <FeatureSection />
+        <WorkFlow />
       </div>
     </div>
   );
