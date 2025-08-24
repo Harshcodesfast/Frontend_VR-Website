6351ca24302076a673dc7fb0bec6a2d988ba4085
Harshcodesfast <harshawalegaonkar@gmail.com>
2025-08-24 16:21:07 +0530

    done

--- src/conponents/footer.tsx
+++ src/conponents/footer.tsx
@@ -3,7 +3,7 @@ import { communityLinks, platformLinks, resourcesLinks } from "../constants";
 const Footer = function () {
   return (
     <footer className="mt-20 border-t py-10 border-neutral-700">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-4 ">
         <div>
           <h3 className="text-md font-semibold mb-4">Resources</h3>
           <ul className="space-y-2">
